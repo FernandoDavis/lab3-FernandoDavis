f9a0b2004fab901e683865e2019f1b19c59a26a1
4015d4 <4015d4@acerola.ece.uprm.edu>
2017-11-15 15:16:17 -0400

    Question 7: Added centralized oval and commented previous borders and lines

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -20,22 +20,33 @@ public class MyPanelClass extends JPanel {
                         int y2 = getHeight() - myInsets.bottom - 1;
                         int width = x2 - x1;
                         int height = y2 - y1;
- 
+                        int dimensionOfOval = 55;
                         //Paint the background
-                        g.setColor(Color.GRAY);
+                        g.setColor(Color.BLACK);
                        
                         //g.fillRect(x1, y1, width, height); fixed so it fills the entire window
                         g.fillRect(x1, y1, width + 1, height + 1);
                       
-                        //Draw a border
-                        g.setColor(Color.YELLOW);
+//                        //Draw a border
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1, y1, width, height);
+//                        //g.drawRect(x1, y1, width + 1, height + 1);  Checking why adding 1 to draw is not recommended
+//                        
+//                        //Adding an internal border
+//                        g.setColor(Color.CYAN);
+//                        g.drawRect(x1+25, y1+25, width-50, height-50);
+//                        
+//                        //First line
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                        //Second line
+//                        g.setColor(Color.GREEN);
+//                        g.drawLine(x1, y2, x2, 0);
                         
-                        g.drawRect(x1, y1, width, height);
-                        //g.drawRect(x1, y1, width + 1, height + 1);  Checking why adding 1 to draw is not recommended
-                        
-                        //Adding an internal border
-                        g.setColor(Color.CYAN);
-                        g.drawRect(x1+25, y1+25, width-50, height-50);
+                        g.setColor(Color.LIGHT_GRAY);
+                        //g.drawOval(x1, y1, width, height); Edited out the border oval to a filled oval
+                        g.fillOval((getWidth()/2) - (dimensionOfOval/2), (getHeight()/2) - (dimensionOfOval/2) , dimensionOfOval, dimensionOfOval);
                         
             }
 }
